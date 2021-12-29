911e9f9e07362fb6f3b8a6818ce309e56636a98f
Fernando <fernando@ibsystem.com>
2021-12-29 16:24:47 -0300

    Add project files.

---
+++ Domain/Products/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace IWantApp.Domain.Products;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        id = Guid.NewGuid();
+    }
+
+    public Guid id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
